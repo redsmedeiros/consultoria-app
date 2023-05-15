9a70d7127e6d0b77107857d164149fb941741b87
Rodolpho <redsmedeiros@gmail.com>
2023-05-15 09:20:51 -0300

    primeiro

---
+++ src/test/java/com/consultoriaService/ConsultoriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultoriaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
