41b0bbcf7df3ff5170e4714668989bfbe76e3d18
Rodolpho <redsmedeiros@gmail.com>
2023-05-19 09:40:47 -0300

    terminando as exceções globais

--- src/main/java/com/consultoriaService/exception/GlobalExceptionHandler.java
+++ src/main/java/com/consultoriaService/exception/GlobalExceptionHandler.java
@@ -1,7 +1,48 @@
 package com.consultoriaService.exception;
 
+import com.consultoriaService.payload.ErrorDetails;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConsultApiException.class)
+    public ResponseEntity<ErrorDetails> consultApiException(ConsultApiException exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EmailAlreadyExists.class)
+    public ResponseEntity<ErrorDetails> emailAlreadyExists(EmailAlreadyExists exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+        
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
